cceb715714d8abc1b2a874095bc28d64cfec3583
MuX <64381665+MuXodious@users.noreply.github.com>
2023-09-04 07:09:43 +0300

    Update main.py
    Replace OpenAI API bits with Ooba' API to allow sending POST requests and recevive a response.

--- main.py
+++ main.py
@@ -44,31 +44,9 @@ class KeywordQueryEventListener(EventListener):
     """
 
     def on_event(self, event, extension):
-        endpoint = "https://api.openai.com/v1/chat/completions"
+        endpoint = "http://localhost:5000/api/v1/generate"
 
         logger.info('Processing user preferences')
-        # Get user preferences
-        try:
-            api_key = extension.preferences['api_key']
-            max_tokens = int(extension.preferences['max_tokens'])
-            frequency_penalty = float(
-                extension.preferences['frequency_penalty'])
-            presence_penalty = float(extension.preferences['presence_penalty'])
-            temperature = float(extension.preferences['temperature'])
-            top_p = float(extension.preferences['top_p'])
-            system_prompt = extension.preferences['system_prompt']
-            line_wrap = int(extension.preferences['line_wrap'])
-            model = extension.preferences['model']
-        # pylint: disable=broad-except
-        except Exception as err:
-            logger.error('Failed to parse preferences: %s', str(err))
-            return RenderResultListAction([
-                ExtensionResultItem(icon=EXTENSION_ICON,
-                                    name='Failed to parse preferences: ' +
-                                    str(err),
-                                    on_enter=CopyToClipboardAction(str(err)))
-            ])
-
         # Get search term
         search_term = event.get_argument()
         logger.info('The search term is: %s', search_term)
@@ -82,39 +60,20 @@ class KeywordQueryEventListener(EventListener):
             ])
 
         # Create POST request
-        headers = {
-            'content-type': 'application/json',
-            'Authorization': 'Bearer ' + api_key
-        }
+
 
         body = {
-            "messages": [
-                {
-                    "role": "system",
-                    "content": system_prompt
-                },
-                {
-                    "role": "user",
-                    "content": search_term
-                }
-            ],
-            "temperature": temperature,
-            "max_tokens": max_tokens,
-            "top_p": top_p,
-            "frequency_penalty": frequency_penalty,
-            "presence_penalty": presence_penalty,
-            "model": model,
+            'prompt': search_term,
+            'max_new_tokens': 46,
+            'auto_max_new_tokens': False,
+            'max_tokens_second': 0,
         }
-        body = json.dumps(body)
 
         logger.info('Request body: %s', str(body))
-        logger.info('Request headers: %s', str(headers))
-
         # Send POST request
         try:
             logger.info('Sending request')
-            response = requests.post(
-                endpoint, headers=headers, data=body, timeout=10)
+            response = requests.post(endpoint, json=body)
         # pylint: disable=broad-except
         except Exception as err:
             logger.error('Request failed: %s', str(err))
@@ -132,29 +91,16 @@ class KeywordQueryEventListener(EventListener):
         #  { message: Message, finish_reason: string, index: number }
         # Message schema
         #  { role: string, content: string }
-        try:
-            response = response.json()
-            choices = response['choices']
-        # pylint: disable=broad-except
-        except Exception as err:
-            logger.error('Failed to parse response: %s', str(response))
-            errMsg = "Unknown error, please check logs for more info"
-            try:
-                errMsg = response['error']['message']
-            except Exception:
-                pass
 
-            return RenderResultListAction([
-                ExtensionResultItem(icon=EXTENSION_ICON,
-                                    name='Failed to parse response: ' +
-                                    errMsg,
-                                    on_enter=CopyToClipboardAction(str(errMsg)))
-            ])
+        response = response.json()
+        choices = response.json()['results'][0]['text']
+        # pylint: disable=broad-except
+
 
         items: list[ExtensionResultItem] = []
         try:
             for choice in choices:
-                message = choice['message']['content']
+                message = choice['text']
                 message = wrap_text(message, line_wrap)
 
                 items.append(ExtensionResultItem(icon=EXTENSION_ICON, name="Assistant", description=message,
